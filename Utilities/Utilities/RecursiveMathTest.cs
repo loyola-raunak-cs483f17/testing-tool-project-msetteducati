ea9d5782f6ac09ff15b677742cae8e2a57708583
msetteducati <michael.setteducati@gmail.com>
2017-12-11 22:18:14 -0500

    Add tests using Theory and InlineData

--- Utilities/Utilities/RecursiveMathTest.cs
+++ Utilities/Utilities/RecursiveMathTest.cs
@@ -5,6 +5,9 @@ namespace Utilities
 {
     public class RecursiveMathTest
     {
+        /**
+         * Test RecursiveAdd() function using Fact.
+         */
         [Fact]
         public void TestRecursiveAdd()
         {
@@ -13,6 +16,22 @@ namespace Utilities
             Assert.Equal(-10, RecursiveMath.RecursiveAdd(-20, 10));
         }
 
+        /**
+         * Test RecursiveAdd() function using Theory and passing
+         * it InlineData.
+         */
+        [Theory]
+        [InlineData(4, 4, 8)]
+        [InlineData(-1, 3, 2)]
+        [InlineData(0, 4, 4)]
+        public void TestRecursiveAdd(int val1, int val2, int expected)
+        {
+            Assert.Equal(expected, RecursiveMath.RecursiveAdd(val1, val2));
+        }
+
+        /**
+         * Test RecursiveSubtract() using Fact.
+         */
         [Fact]
         public void TestRecursiveSubtract()
         {
@@ -21,6 +40,21 @@ namespace Utilities
             Assert.Equal(5, RecursiveMath.RecursiveSubtract(10, 5));
         }
 
+        /**
+         * Test RecursiveSubtract() using Theory.
+         */
+        [Theory]
+        [InlineData(10, 5, 5)]
+        [InlineData(5, 10, -5)]
+        [InlineData(5, -5, 10)]
+        public void TestRecursiveSubtract(int val1, int val2, int expected)
+        {
+            Assert.Equal(expected, RecursiveMath.RecursiveSubtract(val1, val2));
+        }
+
+        /**
+         * Test RecursiveMultiply() using Fact.
+         */
         [Fact]
         public void TestRecursiveMultiply()
         {
@@ -29,6 +63,21 @@ namespace Utilities
             Assert.Equal(0, RecursiveMath.RecursiveMultiply(10, 0));
         }
 
+        /**
+         * Test RecursiveMultiply() using Theory.
+         */
+        [Theory]
+        [InlineData(5, 5, 25)]
+        [InlineData(5, -5, -25)]
+        [InlineData(0, 10, 0)]
+        public void TestRecursiveMultiply(int val1, int val2, int expected)
+        {
+            Assert.Equal(expected, RecursiveMath.RecursiveMultiply(val1, val2));   
+        }
+
+        /**
+         * Test RecursiveDivide() using Fact.
+         */
         [Fact]
         public void TestRecursiveDivide()
         {
@@ -38,12 +87,39 @@ namespace Utilities
             Assert.Equal(0, RecursiveMath.RecursiveDivide(0, 20));
         }
 
+        /**
+         * Test RecursiveDivide() using Theory.
+         */
+        [Theory]
+        [InlineData(25, 5, 5)]
+        [InlineData(22, 5, 4)]
+        [InlineData(12, 4, 3)]
+        public void TestRecursiveDivide(int dividend, int divisor, int expected)
+        {
+            Assert.Equal(expected, RecursiveMath.RecursiveDivide(dividend, divisor));
+        }
+
+        /**
+         * Test RecursiveExponent() using Fact
+         */
         [Fact]
         public void TestRecursiveExponent()
         {
             Assert.Equal(1, RecursiveMath.RecursiveExponent(5, 0));
             Assert.Equal(5, RecursiveMath.RecursiveExponent(5, 1));
             Assert.Equal(16, RecursiveMath.RecursiveExponent(2, 4));
+        }
+
+        /**
+         * Test RecursiveExponent() using Theory.
+         */
+        [Theory]
+        [InlineData(2, 0, 1)]
+        [InlineData(2, 1, 2)]
+        [InlineData(2, 5, 32)]
+        public void TestRecursiveExponent(int num, int exponent, int expected)
+        {
+            Assert.Equal(expected, RecursiveMath.RecursiveExponent(num, exponent));
         }
     }
 }
---
+++ Utilities/Utilities/SortingAlgorithmsTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Utilities
+{
+    public class SortingAlgorithmsTest
+    {
+        public SortingAlgorithmsTest()
+        {
+        }
+    }
+}
